fab15944dbc80894501ec07780aff8cfeb3e92fd
Lakshmadeepan <lakshmadeepan17@gmail.com>
2026-07-30 22:17:33 +0530

    Ires folwer classification system

--- 02.Ires_flower_classification/ires_project.py
+++ 02.Ires_flower_classification/ires_project.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 import joblib
 
 data=pd.read_csv(r"02.Ires_flower_classification\Iris.csv")
-X=data.iloc[:,1:5]
+X=data.iloc[:,1:5]#column
 Y=data.iloc[:,-1]
 encoder=LabelEncoder()
 Y=encoder.fit_transform(Y)
